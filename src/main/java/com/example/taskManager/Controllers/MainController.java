1a83b588dffcfebe3b7d3cc6c574f132a9db5156
Aryan Verma <vermaaryanverma30@gmail.com>
2025-04-01 22:00:55 +0530

    removed unused code

--- src/main/java/com/example/taskManager/Controllers/MainController.java
+++ src/main/java/com/example/taskManager/Controllers/MainController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,11 +23,6 @@ public class MainController {
     @Autowired
     public MainController(UserServiceImpl service){
         this.service = service;
-    }
-
-    @GetMapping("/health-check")
-    public String healthCheck(){
-        return "Ok";
     }
 
     @PostMapping("/register")
